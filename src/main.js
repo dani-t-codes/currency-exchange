9a6d97a6d04c679007add3f07cf1be62749fc3ff
Danielle Thompson <danithompson74@gmail.com>
2020-11-20 15:06:07 -0800

    Purposely fail calling nonexistent json data to test error message

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ function clearFields() {
 }
 //Business Logic
 function getElements(response) {
-  if (response.conversion_rates) {
-    $('#showConversion').text(`The converted rate is ${response.conversion_rates}.`);
+  if (response.conversion_rates.key) {
+    $('#showConversion').text(`The converted rate is ${response.conversion_rates.key}.`);
   } else {
     $('#showErrors').text(`There was an error: ${response}`);
   }
@@ -42,14 +42,12 @@ $(document).ready(function() {
 //dropdown logic testing
 let dropdown = $('#currencyConversion');
 
-
-
 dropdown.append('<option selected="true" disabled>Choose Currency</option>');
 dropdown.prop('selectedIndex', 0);
 
 const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
 
-// Populate dropdown with list of provinces
+// Populate dropdown with list of currency rates
 $.getJSON(url, function (data) {
   $.each(data, function (key, entry) {
     dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
