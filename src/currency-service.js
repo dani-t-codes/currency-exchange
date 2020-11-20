1aa48bc3ddbd073b35739c62418b75ea7767459b
Danielle Thompson <danithompson74@gmail.com>
2020-11-20 12:09:38 -0800

    add eslint disabled line and refactor json input

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,9 +1,10 @@
 export default class CurrencyExchange {
+  // eslint-disable-next-line no-unused-vars
   static async getExchange(currency) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
-        throw Error(response.statusText TODO);
+        throw Error(response.statusText);
       }
       return response.json();
     } catch(error) {
