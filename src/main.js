17315422efda2f08fbbdc6521cdb8d7705327561
Danielle Thompson <danithompson74@gmail.com>
2020-11-20 11:01:19 -0800

    Add error handling to currency-service, add UI logic to main

--- src/main.js
+++ src/main.js
@@ -4,5 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "./currency-service-service.js";
 
-$
+//Business Logic
 CurrencyExchange
+
+
+//Service Logic to currency-service
+
+
+//UI Logic
+$(document).ready(function) {
+  $('#convertButton').click(function() {
+    let currency = $('#currencyConversion').val();
+    $('#currencyConversion').val("");
+  }
+
+
+const conversionMoney = $("#convert-to").val();  //dropdown form pull
+
+}
