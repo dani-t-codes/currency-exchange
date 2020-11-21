bdc824244718d3ebbff71eb372811eaa690ca099
Danielle Thompson <danithompson74@gmail.com>
2020-11-20 16:22:16 -0800

    Refactor out auto populated import statement

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,12 +1,10 @@
-import { type } from "jquery";
-
 export default class CurrencyExchange {
   // eslint-disable-next-line no-unused-vars
   static async getExchange(currency) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
-        throw Error(response.error_type);
+        throw Error(response.name);
       }
       return response.json();
     } catch(error) {
