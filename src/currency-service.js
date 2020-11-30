53d1b9e9e2e299225fb909a30893186ac1630c65
Danielle Thompson <danithompson74@gmail.com>
2020-11-29 19:48:53 -0800

    Correct API call responses

--- src/currency-service.js
+++ src/currency-service.js
@@ -4,11 +4,11 @@ export default class CurrencyExchange {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
-        throw Error(response.name);
+        throw Error(response.statusText);
       }
       return response.json();
     } catch(error) {
-      return error;
+      return error.message;
     }
   }
 }
