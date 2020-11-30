7ded490edbe3e4b6528874b0312be2a9d17368a5
Danielle Thompson <danithompson74@gmail.com>
2020-11-29 19:51:12 -0800

    Work on dropdown fxnality, add currency calculation fxn, work on getElements

--- src/main.js
+++ src/main.js
@@ -9,27 +9,41 @@ function clearFields() {
   $("#showUserInput").text("");
   $("#showConversion").text("");
   $("#showErrors").text("");
+  $("#showRate").text("");
 }
 
 //Business Logic
 function getElements(response) {
+  // $.find({AUD:, BRL:, EGP:, EUR:, IDR:}, calculate(response));
   if (response.conversion_rates.AUD) {
-    $('#showConversion').text(`The conversion rate is ${response.conversion_rates.AUD}.`);
-    //$('#showConversion').text(`The equal dollar amount is (multiplication formula here).`);
+    $('#showRate').text(`The conversion rate is ${response.conversion_rates.AUD}.`);
+    // $('#showConversion').text(`The equal dollar amount in AUD is ${response.conversion_rates.AUD} * (TODO user input)).`);
   } else if (response.conversion_rates.BRL) {
-    $('#showConversion').text(`The conversion rate is ${response.conversion_rates.BRL}.`);
+    $('#showRate').text(`The conversion rate is ${response.conversion_rates.BRL}.`);
   } else if (response.conversion_rates.EGP) {
-    $('#showConversion').text(`The conversion rate is ${response.conversion_rates.EGP}.`);
+    $('#showRate').text(`The conversion rate is ${response.conversion_rates.EGP}.`);
   } else if (response.conversion_rates.EUR) {
-    $('#showConversion').text(`The conversion rate is ${response.conversion_rates.EUR}.`);
+    $('#showRate').text(`The conversion rate is ${response.conversion_rates.EUR}.`);
   } else if (response.conversion_rates.IDR) {
-    $('#showConversion').text(`The conversion rate is ${response.conversion_rates.IDR}.`); 
+    $('#showRate').text(`The conversion rate is ${response.conversion_rates.IDR}.`); 
   } else if (response.conversion_rates.MAR) {
-    $('#showErrors').text(`There was an error: ${response}`);
+    $('#showErrors').text(`There was an error: ${response.result}`);
+  } else if (response === isNaN || response < 0 || response == "") {  //might need to move up b/c is most specific
+    $('#showErrors').text("Not a valid number.");
   } else {
-    $('#showErrors').text(`There was an error: ${response}`);
+    $('.showErrors').text(`There was an error: ${response}`);
   }
 }
+
+// function calculate(response) {
+//   let userDollarInput = parseInt($("input#numberToConvert"));
+//   console.log(parseInt($("input#numberToConvert"))); //NaN
+//   let conversionRate = `${response.conversion_rates}`; //use JSON.parse?? was getting caught in promise
+//   console.log(conversionRate); //now is NaN
+//   let conversion = eval(userDollarInput * conversionRate);
+//   console.log(conversion); //Nonexistent
+//   $('#showConversion').val(conversion);
+// }
 
 //Service Logic to currency-service
 async function makeApiCall(currency) {
@@ -45,23 +59,31 @@ $(document).ready(function() {
     let userDollarInput = $("input#numberToConvert");
     $("#userInput").html(userDollarInput);
 
-    let currency = $('#currencyConversion').val();
+    let currency = $('#currencyConversion').prop(":selected");
+    console.log(currency); //undefined
     clearFields();
     makeApiCall(currency);
+    // calculate();
   });
 });
 
 //dropdown logic testing
-let dropdown = $('#currencyConversion');
+//function getOption() {
+// let dropdown = $('#currencyConversion');
+// let value = dropdown.options[dropdown.selectedIndex].value;
+//$("#elementID :selected").val();
 
-dropdown.append('<option selected="true" disabled>Choose Currency</option>');
-dropdown.prop('selectedIndex', 0);
+// dropdown.prop('selectedIndex', 1);
 
-const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
 
-// Populate dropdown with list of currency rates
-$.getJSON(url, function (data) {
-  $.each(data, function (response, entry) {
-    dropdown.append($('<option></option>').attr('value', response.conversion_rates).text(response.conversion_rate[entry]));
-  });
-});
+
+
+
+
+
+// // Populate dropdown with list of currency rates
+// $.getJSON(response, function (data) {
+//   $.each(data, function (response, entry) {
+//     dropdown.append($('<option></option>').attr('value', response.conversion_rates).text(response.conversion_rate[entry]));
+//   });
+// });
