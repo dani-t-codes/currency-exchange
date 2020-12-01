8c4758c20e03d15c8884531f47e402908493d894
Danielle Thompson <danithompson74@gmail.com>
2020-11-30 19:31:28 -0800

    Change async to promise

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,14 +1,18 @@
 export default class CurrencyExchange {
   // eslint-disable-next-line no-unused-vars
-  static async getExchange(currency) {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    } catch(error) {
-      return error.message;
-    }
+  static getExchange(currency) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
