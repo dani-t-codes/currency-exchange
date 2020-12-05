c1659b16bdbd3ba267b78a43461eeec4d2fe5f17
Danielle Thompson <danithompson74@gmail.com>
2020-12-05 15:24:01 -0800

    Add dropdown-service file & dropdown service fxns to main

---
+++ src/js/dropdown-service.js
@@ -0,0 +1,14 @@
+export default class DropdownExchange {
+  // eslint-disable-next-line no-unused-vars
+  static async getDropdown(dropdownCall) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.result;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import CurrencyExchange from "./currency-service.js";
+import CurrencyExchange from "./js/currency-service.js";
+import DropdownExchange from "./js/dropdown-service.js";
 
 function clearFields() {
   $("#numberToConvert").val("");
@@ -24,27 +25,27 @@ function getElements(response) {
   }
 }
 
-// function getDropDownOpts(call) {
-//   for (let i=1; i <= response.conversion_rates.length; i++) {
-//     let values = Object.entries(response.conversion_rates[i].map((key, value) => `<option id="${key}" value="${value}" disabled>${key}</option>`));
-//     $('option').append(values);
-//   }
+function getDropDownOpts(call) {
+  for (let i=1; i <= call.conversion_rates.length; i++) {
+    let values = Object.entries(call.conversion_rates[i].map((key, value) => `<option id="${key}" value="${value}" disabled>${key}</option>`));
+    $('select').append(values);
+  }
+}
   
 async function makeApiCall(currency) {
   const response = await CurrencyExchange.getExchange(currency);
   getElements(response);
 }
 
-// async function makeApiDropdownCall(dropdownCall) {
-//   const call = await CurrencyExchange.getExchange(dropdownCall);
-//   getDropDownOpts(call);
-// }
+async function makeApiDropdownCall(dropdownCall) {
+  const call = await DropdownExchange.getDropdown(dropdownCall);
+  getDropDownOpts(call);
+}
 
-//UI Logic
 $(document).ready(function() {
-  // $('#dropDownToConvert').click(function() {
-  // makeApiDropdownCall();
-  // });      
+  $('#dropDownToConvert').click(function() {
+    makeApiDropdownCall(dropdownCall); 
+  });      
   $('#convertButton').click(function(event) {
     event.preventDefault();
     let currency = $('#currency').val();
